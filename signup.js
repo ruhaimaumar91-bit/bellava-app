a057c86e9d6a8accfe782973b8dd60dc83a3fae5
ruhaimaumar91-bit <ruhaimaumar91@gmail.com>
2026-04-30 13:23:13 +0000

    bellava

--- signup.js
+++ signup.js
@@ -7,7 +7,7 @@ import {
 import { createClient } from '@supabase/supabase-js';
 import COLORS from './colors';
 
-const supabase = createClient(
+const supabase = createClient( 
   'https://qtvjsvgrojiafyayxrmz.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0dmpzdmdyb2ppYWZ5YXl4cm16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3MTIyOTMsImV4cCI6MjA5MjI4ODI5M30.x3AeUcM2_Ur-NvezP8s4rluf_HM7SZIi0vWeLjZbjiY'
 
